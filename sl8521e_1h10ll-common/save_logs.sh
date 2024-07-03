9ed1b86ef47f644f2d3d95e14a04890d528a4b24
xunmod <116288337+xunmod@users.noreply.github.com>
2024-07-03 15:04:54 +0800

    Disable ccache; try fix log

--- sl8521e_1h10ll-common/save_logs.sh
+++ sl8521e_1h10ll-common/save_logs.sh
@@ -1,14 +1,13 @@
 #!/sbin/busybox sh
 
-echo "======prop" > /data/rec.log
-getprop >> /data/rec.log
-echo "======usb" >> /data/rec.log
-/sbin/busybox find /sys/class/android_usb >> /data/rec.log
-/sbin/busybox find /sys/kernel/config >> /data/rec.log
-echo "======log" >> /data/rec.log
-cat /tmp/recovery.log >> /data/rec.log
-echo "======dmesg" >> /data/rec.log
-busybox dmesg >> /data/rec.log
-echo "======logcat" >> /data/rec.log
+echo "======usb" &> /cache/rec.log
+/sbin/busybox find /sys/class/android_usb 1>>/cache/rec.log 2>>/cache/rec.log 
+/sbin/busybox find /sys/kernel/config 1>>/cache/rec.log 2>>/cache/rec.log 
+echo "======log" 1>>/cache/rec.log 2>>/cache/rec.log 
+cat /tmp/recovery.log 1>>/cache/rec.log 2>>/cache/rec.log 
+echo "======dmesg" 1>>/cache/rec.log 2>>/cache/rec.log 
+busybox dmesg 1>>/cache/rec.log 2>>/cache/rec.log 
+echo "======logcat" 1>>/cache/rec.log 2>>/cache/rec.log 
 sleep 5 && killall logcat &
-logcat >> /data/rec.log
+logcat 1>>/cache/rec.log 2>>/cache/rec.log 
+cp /cache/rec.log /data/
