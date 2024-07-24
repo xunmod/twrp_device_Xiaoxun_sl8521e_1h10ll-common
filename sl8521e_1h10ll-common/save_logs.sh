e2e1ae53e99c1568241c64c7b45cc5ed73f38f95
xunmod <116288337+xunmod@users.noreply.github.com>
2024-07-24 18:37:45 +0800

    adjust init.recovery.usb.rc & log capture

--- sl8521e_1h10ll-common/save_logs.sh
+++ sl8521e_1h10ll-common/save_logs.sh
@@ -1,15 +1,20 @@
 #!/sbin/busybox sh
 
-echo "======usb" &> /rec.log
+busybox dmesg 1>>/early_dmesg 2>>/early_dmesg
+sleep 0.2
+busybox dmesg 1>>/dmesg 2>>/dmesg
+sleep 10
+echo "======usb" >/rec.log
 /sbin/busybox find /sys/class/android_usb 1>>/rec.log 2>>/rec.log 
-/sbin/busybox find /sys/kernel/config 1>>/rec.log 2>>/rec.log 
-echo "======log" 1>>/rec.log 2>>/rec.log 
+/sbin/busybox find /config 1>>/rec.log 2>>/rec.log 
+echo "======log" >>/rec.log 
 cat /tmp/recovery.log 1>>/rec.log 2>>/rec.log 
-echo "======dmesg" 1>>/rec.log 2>>/rec.log 
-busybox dmesg 1>>/rec.log 2>>/rec.log 
-echo "======logcat" 1>>/rec.log 2>>/rec.log 
+echo "======early_dmesg" >>/rec.log
+cat /early_dmesg >>/rec.log
+echo "======dmesg" >>/rec.log
+cat /dmesg >>/rec.log
+echo "======logcat" >>/rec.log 
 sleep 5 && killall logcat &
 logcat 1>>/rec.log 2>>/rec.log
-sleep 10
 cp /rec.log /data/
 cp /rec.log /cache/
