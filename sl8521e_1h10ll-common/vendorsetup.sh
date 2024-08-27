51cd5f833a460914fc2e621a56f9c0b27b5be452
xunmod <116288337+xunmod@users.noreply.github.com>
2024-08-27 18:14:43 +0800

    try fix build

--- sl8521e_1h10ll-common/vendorsetup.sh
+++ sl8521e_1h10ll-common/vendorsetup.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-grep adf_test bootable/recovery/Android.mk &>/dev/null && patch -d bootable/recovery -f -p1 < device/Xiaoxun/sl8521e_1h10ll-common/adf_test.patch
+cd bootable/recovery && git am ../../device/Xiaoxun/sl8521e_1h10ll-common/0001-Add-adf_test.patch || (git am --abort && cd ../.. && exit 1)
+cd ../..
